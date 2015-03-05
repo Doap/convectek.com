f54e90232f55b7037e10601374c87dc32fc46f36
David Menache <dave@doap.com>
2015-03-05 05:00:07 +0000

    minor changes to convectek.com:

---
+++ scripts/make_softlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p /var/www/html/convectek.com/wp-content/themes
+mkdir -p /var/www/html/convectek.com/wp-content/plugins
+ln -s /srv/www/uploads/sinkjuice/convectek.com/uploads/ /var/www/html/convectek.com/wp-content/uploads
+ln -s /srv/www/uploads/sinkjuice/convectek.com/themes/ /var/www/html/convectek.com/wp-content/themes
+ln -s /srv/www/uploads/sinkjuice/convectek.com/plugins/ /var/www/html/convectek.com/wp-content/plugins
+/startup/genvhost.sh convectek.com
+/startup/make-wp-configs convectek.com
+echo "/startup/make-softlinks.sh just ran on `curl -s http://169.254.169.254/latest/meta-data/public-ipv4`." | tee /tmp/startup.log
---
+++ scripts/makedirs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#mkdir -p /var/www/html/convectek.com/wp-content/themes
+#mkdir -p /var/www/html/convectek.com/wp-content/plugins
+echo ok 
